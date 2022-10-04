cc515b62d99f1844c67d220f315024b19520b2a5
Emily Serven <emily.e.serven@gmail.com>
2022-10-04 13:27:19 -0400

    Solution for is_prime exercise.

--- is_prime.lua
+++ is_prime.lua
@@ -1,9 +1,19 @@
--- get user input of a number, num
+print("Enter a number.")
+num = io.read()
 
--- ensure num is a number
+function check_if_prime(num)
+    tonumber(num)
+    is_prime = false
+    for i = 1, (num - 1) do
+        prime_check = num % i
+        if prime_check == 0 and i ~= 1 then
+            is_prime = true
+            print("NOT prime!")
+        end
+    end
+    if is_prime == false then
+        print("Prime!")
+    end
+end
 
--- function, is_prime input num
--- -- for numbers between 1 and num...
--- -- -- modulo and check if the result is 0
--- -- -- -- if it is 0, and number being checked is NOT 1, it is not prime and should RETURN something to indicate so
--- -- -- -- if it is not 0 or if the number being checked is 1, keep going
+check_if_prime(num)
