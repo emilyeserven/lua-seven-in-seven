7fb8dc28869696dae9ffa7740dc4ff30996ba58f
Emily Serven <emily.e.serven@gmail.com>
2022-10-04 13:05:33 -0400

    Pseudocode for is_prime exercise

---
+++ is_prime.lua
@@ -0,0 +1,9 @@
+-- get user input of a number, num
+
+-- ensure num is a number
+
+-- function, is_prime input num
+-- -- for numbers between 1 and num...
+-- -- -- modulo and check if the result is 0
+-- -- -- -- if it is 0, and number being checked is NOT 1, it is not prime and should RETURN something to indicate so
+-- -- -- -- if it is not 0 or if the number being checked is 1, keep going
