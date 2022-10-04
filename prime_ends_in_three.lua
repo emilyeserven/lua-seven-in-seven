d0af94dd9898674dc5ae694916398e3e8175defe
Emily Serven <emily.e.serven@gmail.com>
2022-10-04 13:56:57 -0400

    Better solution for prime_ends_in_three exercise

--- prime_ends_in_three.lua
+++ prime_ends_in_three.lua
@@ -1,35 +1,28 @@
--- TODO: Do a module import/export thing.
-
 n = io.read()
 
 function ends_in_three(num)
-    --print("checking if "..num.." ends in three")
     tostring(num)
     last_num = string.sub(num, -1)
     if last_num == "3" then
-        --print("it does!")
         return true
     else
-        --print("it does not!")
         return false
     end
 end
 
 function check_if_prime(num)
-    --print("check if "..num.." is prime")
     tonumber(num)
     is_prime = true
+    -- Run through and check if it's prime first
     for i = 1, (num - 1) do
         prime_check = num % i
         if prime_check == 0 and i ~= 1 then
             is_prime = false
             return
-        else
-            is_prime = true
         end
     end
+    -- If it is prime, then do this...
     if is_prime == true then
-        --print(num .. "is prime, so we check if it ends in 3")
         check_three = ends_in_three(num)
         if check_three == true then
             print(num)
@@ -38,7 +31,6 @@ function check_if_prime(num)
 end
 
 function check_n_primes(n)
-    --print("check n primes")
     for i = 1, n do
         check_if_prime(i)
     end
